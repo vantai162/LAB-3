524342126ba25b35b99cdbeb93478d874a68e7a9
vantai162 <157353141+vantai162@users.noreply.github.com>
2024-10-29 21:30:19 +0700

    Add files via upload

---
+++ LAB3CLASS.sql
@@ -0,0 +1,141 @@
+﻿--CAU 1--
+USE QuanLyBanHang
+GO
+
+--12.Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20. --
+SELECT SOHD
+FROM CTHD
+WHERE (MASP = 'BB01' OR MASP = 'BB02') AND (SL BETWEEN 10 AND 20)
+
+--13. Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20. --
+SELECT SOHD
+FROM CTHD
+WHERE (MASP = 'BB01' AND SL BETWEEN 10 AND 20)
+   OR (MASP = 'BB02' AND SL BETWEEN 10 AND 20)
+GROUP BY SOHD
+
+--CAU 2--
+
+USE QuanLyHocVu
+GO
+
+--1.Tăng hệ số lương thêm 0.2 cho những giáo viên là trưởng khoa.--
+UPDATE GIAOVIEN
+SET HESO = HESO + 0.2
+WHERE MAGV IN (SELECT TRGKHOA FROM KHOA);
+
+--2.Cập nhật giá trị điểm trung bình tất cả các môn học (DIEMTB) của mỗi học viên (tất cả các môn học đều có hệ số 1 và nếu học viên thi một môn nhiều lần, chỉ lấy điểm của lần thi sau cùng).--
+UPDATE HocVien
+SET DiemTB =
+(
+	SELECT AVG(Diem)
+	FROM KetQuaThi
+	WHERE LanThi = (SELECT MAX(LanThi) FROM KetQuaThi KQ WHERE MaHV = KetQuaThi.MaHV GROUP BY MaHV)
+	GROUP BY MaHV
+	HAVING MaHV = HocVien.MaHV
+)
+
+--3.Cập nhật giá trị cho cột GHICHU là “Cam thi” đối với trường hợp: học viên có một môn bất kỳ thi lần thứ 3 dưới 5 điểm--
+UPDATE HOCVIEN
+SET GHICHU = N'Cam thi'
+WHERE EXISTS 
+(
+    SELECT 1
+    FROM KETQUATHI 
+    WHERE KETQUATHI.MAHV = HOCVIEN.MAHV
+      AND KETQUATHI.LANTHI = 3
+      AND KETQUATHI.DIEM < 5
+)
+
+--4--
+UPDATE HOCVIEN
+SET XEPLOAI = CASE
+    WHEN DIEMTB >= 9 THEN 'XS'
+    WHEN DIEMTB >= 8 AND DIEMTB < 9 THEN 'G'
+    WHEN DIEMTB >= 6.5 AND DIEMTB < 8 THEN 'K'
+    WHEN DIEMTB >= 5 AND DIEMTB < 6.5 THEN 'TB'
+    WHEN DIEMTB < 5 THEN 'Y'
+END
+
+--CAU 3--
+--6.Tìm tên những môn học mà giáo viên có tên “Tran Tam Thanh” dạy trong học kỳ 1 năm 2006.--
+SELECT MONHOC.MAMH
+FROM  MONHOC
+JOIN GIANGDAY ON GIANGDAY.MAMH = MONHOC.MAMH
+JOIN GIAOVIEN ON GIANGDAY.MAGV = GIAOVIEN.MAGV
+WHERE HOTEN = 'Tran Tam Thanh' AND HOCKY = 1 AND NAM = 2006
+
+--7.tìm những môn học (mã môn học, tên môn học) mà giáo viên chủ nhiệm lớp “K11” dạy trong học kỳ 1 năm 2006.--
+SELECT DISTINCT MONHOC.MAMH, MONHOC.TENMH
+FROM MONHOC 
+JOIN GIANGDAY ON MONHOC.MAMH = GIANGDAY.MAMH
+JOIN LOP ON GIANGDAY.MALOP = LOP.MALOP
+WHERE LOP.MALOP = 'K11'
+    AND GIANGDAY.HOCKY = 1
+    AND GIANGDAY.NAM = 2006
+
+--8.Tìm họ tên lớp trưởng của các lớp mà giáo viên có tên “Nguyen To Lan” dạy môn “Co So Du Lieu”.--
+SELECT HOCVIEN.HO,HOCVIEN.TEN
+FROM HOCVIEN
+JOIN LOP ON HOCVIEN.MAHV = LOP.TRGLOP
+JOIN GIANGDAY ON GIANGDAY.MALOP = LOP.MALOP
+JOIN GIAOVIEN ON GIAOVIEN.MAGV = GIANGDAY.MAGV
+JOIN MONHOC ON GIANGDAY.MAMH = MONHOC.MAMH
+WHERE GIAOVIEN.HOTEN = 'Nguyen To Lan' AND MONHOC.TENMH = 'Co So Du Lieu'
+
+--9.In ra danh sách những môn học (mã môn học, tên môn học) phải học liền trước môn “Co So Du Lieu”.--
+SELECT DIEUKIEN.MAMH_TRUOC, MONHOC.TENMH
+FROM DIEUKIEN 
+JOIN MONHOC ON DIEUKIEN.MAMH_TRUOC = MONHOC.MAMH
+WHERE DIEUKIEN.MAMH = (SELECT MAMH 
+                 FROM MONHOC 
+                 WHERE TENMH = 'Co So Du Lieu');
+
+--10.Môn “Cau Truc Roi Rac” là môn bắt buộc phải học liền trước những môn học (mã môn học, tên môn học) nào. --
+SELECT MONHOC.MAMH
+FROM MONHOC
+JOIN DIEUKIEN ON DIEUKIEN.MAMH = MONHOC.MAMH
+WHERE	DIEUKIEN.MAMH_TRUOC = (SELECT MAMH 
+                        FROM MONHOC 
+                        WHERE TENMH = 'Cau Truc Roi Rac')
+
+--Cau 04--
+USE QuanLyBanHang
+GO
+
+-- 14. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.--
+SELECT SANPHAM.MASP,TENSP
+FROM SANPHAM
+JOIN CTHD ON CTHD.MASP = SANPHAM.MASP
+JOIN HOADON ON HOADON.SOHD = CTHD.SOHD
+WHERE SANPHAM.NUOCSX = 'Trung Quoc' OR HOADON.NGHD = '1/1/2007'
+
+-- 15. In ra danh sách các sản phẩm (MASP,TENSP) không bán được. --
+SELECT SANPHAM.MASP,TENSP
+FROM SANPHAM
+LEFT JOIN CTHD ON CTHD.MASP = SANPHAM.MASP
+WHERE CTHD.MASP IS NULL
+
+-- 16. In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006. --
+SELECT SANPHAM.MASP,TENSP
+FROM SANPHAM
+LEFT JOIN CTHD ON CTHD.MASP = SANPHAM.MASP
+LEFT JOIN HOADON ON HOADON.SOHD = CTHD.SOHD
+WHERE YEAR(NGHD) != 2006 OR NGHD IS NULL
+-- 17. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán được trong năm 2006. --
+SELECT SANPHAM.MASP,TENSP
+FROM SANPHAM
+LEFT JOIN CTHD ON CTHD.MASP = SANPHAM.MASP
+LEFT JOIN HOADON ON HOADON.SOHD = CTHD.SOHD
+WHERE (YEAR(NGHD) != 2006 OR NGHD IS NULL) AND (NUOCSX = 'Trung Quoc')
+-- 18. Tìm số hóa đơn trong năm 2006 đã mua ít nhất tất cả các sản phẩm do Singapore sản xuất. --
+SELECT HOADON.SOHD
+FROM HOADON 
+JOIN CTHD ON HOADON.SOHD = CTHD.SOHD
+JOIN SANPHAM ON CTHD.MASP = SANPHAM.MASP
+WHERE HOADON.NGHD BETWEEN '2006-01-01' AND '2006-12-31'
+  AND SANPHAM.NUOCSX = 'Singapore'
+GROUP BY HOADON.SOHD
+HAVING COUNT(DISTINCT SANPHAM.MASP) = (SELECT COUNT(*) 
+                                 FROM SANPHAM 
+                                 WHERE NUOCSX = 'Singapore')
---
+++ Lab-3.sql
@@ -0,0 +1,150 @@
+
+-- 8. Hiển thị tên và cấp độ của tất cả các kỹ năng của chuyên gia có MaChuyenGia là 1.
+SELECT TenKyNang,CapDo
+FROM ChuyenGia_KyNang
+JOIN KyNang ON ChuyenGia_KyNang.MaKyNang = KyNang.MaKyNang
+WHERE MaChuyenGia = 1
+
+-- 9. Liệt kê tên các chuyên gia tham gia dự án có MaDuAn là 2.
+SELECT HoTen
+FROM ChuyenGia
+JOIN ChuyenGia_DuAn ON ChuyenGia_DuAn.MaChuyenGia = ChuyenGia.MaChuyenGia
+WHERE MaDuAn = 2
+
+-- 10. Hiển thị tên công ty và tên dự án của tất cả các dự án.
+SELECT TenCongTy,TenDuAn
+FROM CongTy
+JOIN DuAn ON CongTy.MaCongTy = DuAn.MaCongTy
+-- 11. Đếm số lượng chuyên gia trong mỗi chuyên ngành.
+SELECT ChuyenNganh, COUNT(MaChuyenGia) AS SoLuongChuyenGia
+FROM ChuyenGia
+GROUP BY ChuyenNganh
+
+
+-- 12. Tìm chuyên gia có số năm kinh nghiệm cao nhất.
+SELECT TOP 1 HoTen, NamKinhNghiem
+FROM ChuyenGia
+ORDER BY NamKinhNghiem DESC
+
+-- 13. Liệt kê tên các chuyên gia và số lượng dự án họ tham gia.
+SELECT HoTen,COUNT(DUAN.MaDuAn) AS SOLUONGDUAN
+FROM ChuyenGia
+JOIN ChuyenGia_DuAn ON ChuyenGia_DuAn.MaChuyenGia = ChuyenGia.MaChuyenGia
+JOIN DuAn ON ChuyenGia_DuAn.MaDuAn = DuAn.MaDuAn
+GROUP BY HoTen
+
+-- 14. Hiển thị tên công ty và số lượng dự án của mỗi công ty.
+SELECT TenCongTy,COUNT(*) AS SOLUONGDUAN
+FROM CongTy
+JOIN DuAn ON CongTy.MaCongTy = DuAn.MaCongTy
+GROUP BY TenCongTy
+
+-- 15. Tìm kỹ năng được sở hữu bởi nhiều chuyên gia nhất.
+SELECT TenKyNang,COUNT(MaChuyenGia) AS SoChuyenGia
+FROM KyNang
+JOIN ChuyenGia_KyNang ON ChuyenGia_KyNang.MaKyNang = KyNang.MaKyNang
+GROUP BY TenKyNang
+
+-- 16. Liệt kê tên các chuyên gia có kỹ năng 'Python' với cấp độ từ 4 trở lên.
+SELECT HoTen
+FROM ChuyenGia
+JOIN ChuyenGia_KyNang ON ChuyenGia_KyNang.MaChuyenGia = ChuyenGia.MaChuyenGia
+JOIN KyNang ON KyNang.MaKyNang = ChuyenGia_KyNang.MaKyNang
+WHERE TenKyNang = 'Python' AND CapDo >= 4
+
+-- 17. Tìm dự án có nhiều chuyên gia tham gia nhất.
+SELECT TenDuAn,COUNT(ChuyenGia_DuAn.MaChuyenGia) AS SOLUONGCHUYENGIA
+FROM DuAn
+JOIN ChuyenGia_DuAn ON ChuyenGia_DuAn.MaDuAn = DuAn.MaDuAn
+JOIN ChuyenGia ON ChuyenGia.MaChuyenGia = ChuyenGia_DuAn.MaChuyenGia
+GROUP BY TenDuAn
+
+-- 18. Hiển thị tên và số lượng kỹ năng của mỗi chuyên gia.
+SELECT HoTen,COUNT(KYNANG.MaKyNang) AS SOLUONGKYNANG
+FROM ChuyenGia
+JOIN ChuyenGia_KyNang ON ChuyenGia_KyNang.MaChuyenGia = ChuyenGia.MaChuyenGia
+JOIN  KyNang ON KyNang.MaKyNang = ChuyenGia_KyNang.MaKyNang
+GROUP BY HoTen
+
+-- 19. Tìm các cặp chuyên gia làm việc cùng dự án.
+SELECT A.MaChuyenGia AS MaChuyenGia1, B.MaChuyenGia AS MaChuyenGia2, A.MaDuAn
+	FROM ChuyenGia_DuAn A
+	JOIN ChuyenGia_DuAn B ON A.MaDuAn = B.MaDuAn
+	WHERE A.MaChuyenGia < B.MaChuyenGia;
+
+-- 20. Liệt kê tên các chuyên gia và số lượng kỹ năng cấp độ 5 của họ.
+SELECT HoTen,COUNT(KYNANG.MaKyNang) AS SOLUONGKYNANG
+FROM ChuyenGia
+JOIN ChuyenGia_KyNang ON ChuyenGia_KyNang.MaChuyenGia = ChuyenGia.MaChuyenGia
+JOIN  KyNang ON KyNang.MaKyNang = ChuyenGia_KyNang.MaKyNang
+WHERE CAPDO = 5
+GROUP BY HoTen
+
+-- 21. Tìm các công ty không có dự án nào.
+SELECT TenCongTy
+FROM CongTy
+LEFT JOIN DuAn ON DuAn.MaCongTy = CongTy.MaCongTy
+WHERE MaDuAn IS NULL
+
+-- 22. Hiển thị tên chuyên gia và tên dự án họ tham gia, bao gồm cả chuyên gia không tham gia dự án nào.
+SELECT  HoTen,TenDuAn
+FROM ChuyenGia
+LEFT JOIN ChuyenGia_DuAn ON ChuyenGia.MaChuyenGia = ChuyenGia_DuAn.MaChuyenGia
+LEFT JOIN DuAn ON DuAn.MaDuAn = ChuyenGia_DuAn.MaDuAn
+
+
+-- 23. Tìm các chuyên gia có ít nhất 3 kỹ năng.
+SELECT ChuyenGia.MaChuyenGia, ChuyenGia.HoTen, COUNT(ChuyenGia_KyNang.MaKyNang) AS SoLuongKyNang
+FROM CHUYENGIA 
+JOIN ChuyenGia_KyNang ON ChuyenGia.MaChuyenGia = ChuyenGia_KyNang.MaChuyenGia
+GROUP BY ChuyenGia.MaChuyenGia, ChuyenGia.HoTen
+HAVING COUNT(ChuyenGia_KyNang.MaKyNang) >= 3;
+
+-- 24. Hiển thị tên công ty và tổng số năm kinh nghiệm của tất cả chuyên gia trong các dự án của công ty đó.
+SELECT TenCongTy, SUM(NAMKINHNGHIEM) AS TongNamKinhNghiem
+FROM CONGTY 
+JOIN DUAN  ON CongTy.MaCongTy = DuAn.MaCongTy
+JOIN ChuyenGia_DuAn ON DuAn.MaDuAn = ChuyenGia_DuAn.MaDuAn
+JOIN CHUYENGIA  ON ChuyenGia_DuAn.MaChuyenGia = ChuyenGia.MaChuyenGia
+GROUP BY CongTy.TenCongTy
+
+-- 25. Tìm các chuyên gia có kỹ năng 'Java' nhưng không có kỹ năng 'Python'.
+SELECT DISTINCT *
+FROM ChuyenGia_KyNang
+JOIN KyNang  ON ChuyenGia_KyNang.MaKyNang = KyNang.MaKyNang
+WHERE KyNang.TenKyNang = 'Java'
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM KyNang 
+      WHERE ChuyenGia_KyNang.MaKyNang = KyNang.MaKyNang
+        AND KyNang.TenKyNang = 'Python'
+  )
+
+-- 76. Tìm chuyên gia có số lượng kỹ năng nhiều nhất.
+SELECT TOP 1 ChuyenGia.MaChuyenGia, ChuyenGia.HoTen, COUNT(ChuyenGia_KyNang.MaKyNang) AS SoLuongKyNang
+FROM CHUYENGIA 
+JOIN ChuyenGia_KyNang ON ChuyenGia.MaChuyenGia = ChuyenGia_KyNang.MaChuyenGia
+GROUP BY ChuyenGia.MaChuyenGia, ChuyenGia.HoTen
+ORDER BY SoLuongKyNang DESC
+
+
+-- 77. Liệt kê các cặp chuyên gia có cùng chuyên ngành.
+SELECT cg1.MaChuyenGia AS ChuyenGia1, cg2.MaChuyenGia AS ChuyenGia2, cg1.ChuyenNganh
+FROM ChuyenGia cg1
+JOIN ChuyenGia cg2 ON cg1.ChuyenNganh = cg2.ChuyenNganh AND cg1.MaChuyenGia < cg2.MaChuyenGia;
+
+-- 78. Tìm công ty có tổng số năm kinh nghiệm của các chuyên gia trong dự án cao nhất.
+SELECT TOP 1 TenCongTy, SUM(NAMKINHNGHIEM) AS TongNamKinhNghiem
+FROM CONGTY 
+JOIN DUAN  ON CongTy.MaCongTy = DuAn.MaCongTy
+JOIN ChuyenGia_DuAn ON DuAn.MaDuAn = ChuyenGia_DuAn.MaDuAn
+JOIN CHUYENGIA  ON ChuyenGia_DuAn.MaChuyenGia = ChuyenGia.MaChuyenGia
+GROUP BY CongTy.TenCongTy
+ORDER BY TongNamKinhNghiem DESC
+
+-- 79. Tìm kỹ năng được sở hữu bởi tất cả các chuyên gia.
+SELECT TenKyNang
+FROM  KyNang
+JOIN ChuyenGia_KyNang ON ChuyenGia_KyNang.MaKyNang = KyNang.MaKyNang
+GROUP BY TenKyNang
+HAVING COUNT(DISTINCT ChuyenGia_KyNang.MaChuyenGia) = (SELECT COUNT(*) FROM ChuyenGia_KyNang);
